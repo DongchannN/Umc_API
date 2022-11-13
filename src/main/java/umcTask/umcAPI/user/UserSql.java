7f390132b5beb2a4863e0c44efc6435905e35dca
DongchannN <ldc991104@gmail.com>
2022-11-13 12:39:37 +0900

    Update sql statements. table name user to User.

--- src/main/java/umcTask/umcAPI/user/UserSql.java
+++ src/main/java/umcTask/umcAPI/user/UserSql.java
@@ -1,15 +1,15 @@
 package umcTask.umcAPI.user;
 
 public class UserSql {
-    public static final String SELECT = "SELECT userIdx, userId, userName, userPw, createdAt, status FROM user WHERE 1=1";
+    public static final String SELECT = "SELECT userIdx, userId, userName, userPw, createdAt, status FROM User WHERE 1=1";
 
     public static final String STATUS_CODE = " AND status = :status;";
 
-    public static final String INSERT = "INSERT INTO user (userId, userName, userPw) values (:userId, :userName, :userPw);";
+    public static final String INSERT = "INSERT INTO User (userId, userName, userPw) values (:userId, :userName, :userPw);";
 
-    public static final String DELETE = "DELETE FROM user WHERE 1=1";
+    public static final String DELETE = "DELETE FROM User WHERE 1=1";
 
     public static final String ID_CONDITION = " AND userIdx = :userIdx";
 
-    public static final String UPDATE = "UPDATE user SET userName = :userName, userPw = :userPw WHERE 1=1";
+    public static final String UPDATE = "UPDATE User SET userName = :userName, userPw = :userPw WHERE 1=1";
 }
